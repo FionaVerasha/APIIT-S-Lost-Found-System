86f664cb675abf5b7a3232d83217f38c7f83d828
Fiona <fionaverashaapiit@gmail.com>
2025-11-18 11:54:51 +0530

    login and register page

--- db.php
+++ db.php
@@ -8,6 +8,7 @@ $dbname = "if0_40440306_lost_and_found";
 $conn = new mysqli($host, $user, $pass, $dbname);
 
 if ($conn->connect_error) {
+    
     die("Database connection failed: " . $conn->connect_error);
 }
 ?>
